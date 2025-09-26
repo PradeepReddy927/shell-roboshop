97c96435456e477b195d2331edb98b37501c9ea4
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-09-26 18:41:05 +0530

    new roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -11,7 +11,7 @@ do
    # GET Private IP
    if [ $instance != "frontend" ]; then
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-      RECORD_NAME="$insatnce.$DOMAIN_NAME" # # mongodb.dawsdevops86.fun
+      RECORD_NAME="$instance.$DOMAIN_NAME" # # mongodb.dawsdevops86.fun
    else
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
       RECORD_NAME="$DOMAIN_NAME" # dawsdevops86.fun
