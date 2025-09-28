93f0b27565c418b95d73828b7e04ef024050f72b
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-09-28 17:32:49 +0530

    some messages

--- catalogue.sh
+++ catalogue.sh
@@ -76,7 +76,13 @@ VALIDATE $? "Copy mongo repo"
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
-mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
-VALIDATE $? "Load catalogue products"
+INDEX=$(mongosh mongodb.dawsdevops86.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -lt 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load catalogue products"
+else
+    echo -e "catalogue products already loaded ... $Y SKIPPING $N"
+fi
+        
 systemctl restart catalogue
 VALIDATE $? "Restarted catalogue"
