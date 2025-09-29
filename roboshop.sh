504adf0db1d29e946d3d7b69b61988ba850ab1f7
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-09-29 14:46:46 +0530

    some messages

--- roboshop.sh
+++ roboshop.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-01bc6f6c9dc6d7fc5"
-ZONE_ID="Z04861221NEYV46WIDU4A"
-DOMAIN_NAME="dawsdevops86.fun"
+SG_ID="sg-01bc6f6c9dc6d7fc5" # replace with your SG ID
+ZONE_ID="Z04861221NEYV46WIDU4A" # replace with your ID
+DOMAIN_NAME="daws86s.fun"
 
 for instance in $@ # mongodb redis mysql
 do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-  INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
-
-   # GET Private IP
-   if [ $instance != "frontend" ]; then
-      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-      RECORD_NAME="$instance.$DOMAIN_NAME" # # mongodb.dawsdevops86.fun
-   else
-      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-      RECORD_NAME="$DOMAIN_NAME" # dawsdevops86.fun
-   fi
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
+    fi
 
     echo "$instance: $IP"
 
-  aws route53 change-resource-record-sets \
+    aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
@@ -39,4 +38,4 @@ do
         }]
     }
     '
-  done
+done
